3b73b08a7f600bccd03d623ff29c17464ff6591e
Vladimir <rakhmanov-v@ya.ru>
2024-11-28 15:18:00 +0400

    Remove unnecessary files

---
+++ visualise_graph.py
@@ -0,0 +1,18 @@
+# Do not work properly (
+
+from graphviz import Digraph
+from conv_async_graph_operator3 import Agent  # Импортируем агента
+
+agent = Agent()
+
+
+def visualize_graph(agent_in):
+    dot = Digraph()
+    for node_name, node in agent_in.graph.nodes.items():
+        dot.node(node_name)
+        for neighbor in node.successors:
+            dot.edge(node_name, neighbor)
+    dot.render("agent_graph", format="png", view=True)  # Откроет граф и сохранит его как PNG
+
+
+visualize_graph(agent)
